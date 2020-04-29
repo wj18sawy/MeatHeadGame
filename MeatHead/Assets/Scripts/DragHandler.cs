96342949100bdd490eed6eaee887eb4ab6ab5a81
Wyatt Sawyer <thesoya@gmail.com>
2020-04-29 19:28:00 -0400

    Dragging functionality
    some dragging functionality added with the drag handler script

---
+++ MeatHead/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler 
+{
+    Vector3 startpos;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 newpos = Input.mousePosition - startpos;
+        gameObject.transform.localPosition = newpos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
