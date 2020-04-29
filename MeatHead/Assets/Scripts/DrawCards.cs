68963c1c25530ea153bd28e7a8958185853ed902
Wyatt Sawyer <thesoya@gmail.com>
2020-04-29 18:45:36 -0400

    Initial Commit
    new project

---
+++ MeatHead/Assets/Scripts/DrawCards.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        GameObject playerCard = Instantiate(Card1, new Vector3(0, 0, 0), Quaternion.identity);
+        playerCard.transform.SetParent(PlayerArea.transform, false);
+    }
+}
