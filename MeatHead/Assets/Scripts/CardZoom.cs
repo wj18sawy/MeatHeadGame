fb85d759e1a1d8e67c3882885c06e6f13835ce64
Wyatt Sawyer <thesoya@gmail.com>
2020-05-05 23:12:38 -0400

    card zoom
    Cards now zoom in when hovering over them

---
+++ MeatHead/Assets/Scripts/CardZoom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardZoom : MonoBehaviour
+{
+    public GameObject Canvas;
+
+    private GameObject zoomCard;
+
+    public void Awake()
+    {
+        Canvas = GameObject.Find("Main Canvas");    
+    }
+
+    public void OnHoverEnter()
+    {
+        zoomCard = Instantiate(gameObject, new Vector2(100, 200), Quaternion.identity);
+        zoomCard.transform.SetParent(Canvas.transform, true);
+        zoomCard.layer = LayerMask.NameToLayer("Zoom");
+
+        RectTransform rect = zoomCard.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(75, 116); // zoom in 1.5x
+    }
+
+    public void OnHoverExit()
+    {
+        Destroy(zoomCard);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
