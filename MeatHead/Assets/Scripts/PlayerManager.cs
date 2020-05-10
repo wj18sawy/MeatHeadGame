0ca2a9ba5468f497067a6a8359d8bc562ca69c05
Wyatt Sawyer <thesoya@gmail.com>
2020-05-10 17:38:06 -0400

    Mirror multiplayer draw cards
    Networking basics set up with clients and hosts, players can draw cards and see their enemies draw cards

---
+++ MeatHead/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerManager : NetworkBehaviour
+{
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+    public GameObject EnemyArea;
+    public GameObject DropZone;
+
+    List<GameObject> cards = new List<GameObject>();
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        PlayerArea = GameObject.Find("PlayerArea");
+        EnemyArea = GameObject.Find("EnemyArea");
+        DropZone = GameObject.Find("DropZone");
+    }
+
+    [Server]
+    public override void OnStartServer()
+    {
+        cards.Add(Card1);
+        cards.Add(Card2);
+    }
+
+    [Command]
+    public void CmdDealCards()
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            GameObject card = Instantiate(cards[Random.Range(0, cards.Count)], new Vector2(0, 0), Quaternion.identity);
+            NetworkServer.Spawn(card, connectionToClient);
+            RpcShowCard(card, "Dealt");
+        }
+    }
+
+    //Might need this in the future
+    //void DealCards()
+    //{
+    //    if (hasAuthority)
+    //    {
+    //        CmdDealCards();
+    //    }
+    //}
+
+    [ClientRpc]
+    void RpcShowCard(GameObject card, string type)
+    {
+        if (type == "Dealt")
+        {
+            if (hasAuthority)
+            {
+                card.transform.SetParent(PlayerArea.transform, false);
+            }
+            else
+            {
+                card.transform.SetParent(EnemyArea.transform, false);
+            }
+        }
+    }
+}
