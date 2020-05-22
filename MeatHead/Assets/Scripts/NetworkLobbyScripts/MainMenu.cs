7d10b0c8b8e3bd41c2ee7c1c1600f864d5b7cb29
Wyatt Sawyer <thesoya@gmail.com>
2020-05-22 14:50:18 -0400

    beginning network lobby
    changed the original network manager prefab, added scripts for networking. Following tutorial - will link in read me

---
+++ MeatHead/Assets/Scripts/NetworkLobbyScripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.NetworkLobbyScripts
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private NetworkManagerLobby networkManager = null;
+
+        [Header("UI")]
+        [SerializeField] private GameObject landingPagePanel = null;
+
+        public void HostLobby()
+        {
+            networkManager.StartHost();
+
+            landingPagePanel.SetActive(false);
+        }
+    }
+}
